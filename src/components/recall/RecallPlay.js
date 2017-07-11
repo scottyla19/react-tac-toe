a284b99065c7457cbaefd41a3702d738d6a9c999
Scott <scottyla19@gmail.com>
2017-07-10 21:00:08 -0600

    fixed main menu to use list and added recall component

---
+++ src/components/recall/RecallPlay.js
@@ -0,0 +1,8 @@
+import React from "react";
+import MuiThemeProvider from "material-ui/styles/MuiThemeProvider";
+
+export default class RecallGame extends React.Component {
+  render() {
+    return <div> Hello Recall </div>;
+  }
+}
--- src/index.js
+++ src/index.js
@@ -1,12 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-import StartMenu from "./components/StartMenu";
-import TicTacGame from "./components/SinglePlayerGame";
+import StartMenu from "./components/tic-tac-toe/StartMenu";
+import RecallGame from "./components/recall/RecallPlay";
+
 import injectTapEventPlugin from "react-tap-event-plugin";
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import RaisedButton from "material-ui/RaisedButton";
 import MuiThemeProvider from "material-ui/styles/MuiThemeProvider";
+import Avatar from "material-ui/Avatar";
+import { List, ListItem } from "material-ui/List";
+import RightArrow from "material-ui/svg-icons/navigation/arrow-forward";
 
 // Needed for onTouchTap
 // http://stackoverflow.com/a/34015469/988941
@@ -16,12 +20,22 @@ injectTapEventPlugin();
 function Home() {
   return (
     <MuiThemeProvider>
-      <RaisedButton
-        label="React-Tac-Toe"
-        labelColor="#00d8ff"
-        backgroundColor="#222"
-        href="/tic-tac-toe"
-      />
+      <div style={{ width: "40vw" }}>
+        <List>
+          <ListItem
+            primaryText="React-Tac-Toe"
+            leftAvatar={<Avatar src="/react-tac-toe-icon-64.png" />}
+            rightIcon={<RightArrow />}
+            href="/tic-tac-toe"
+          />
+          <ListItem
+            primaryText="React Recall"
+            leftAvatar={<Avatar src="/react-recall-icon-192.png" />}
+            rightIcon={<RightArrow />}
+            href="/recall"
+          />
+        </List>
+      </div>
     </MuiThemeProvider>
   );
 }
@@ -31,6 +45,7 @@ const App = () =>
     <div>
       <Route exact path="/" component={Home} />
       <Route path="/tic-tac-toe" component={StartMenu} />
+      <Route path="/recall" component={RecallGame} />
     </div>
   </Router>;
 
